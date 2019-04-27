660a8b1ff00356f7caba99f9bdf80375bf73b765
Ilona <ilonabarashkina@gmail.com>
2019-04-27 13:41:42 -0400

    Updated the Add Post Form

--- src/app/postForms/postForms.component.ts
+++ src/app/postForms/postForms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output, EventEmitter} from '@angular/core';
+import { Component,Input,Output, EventEmitter} from '@angular/core';
 
 export interface Posting {
   subject:string;
@@ -10,41 +10,26 @@ export interface Posting {
   templateUrl: './postForms.component.html',
   styleUrls: ['./postForms.component.css']
 })
-export class PostFormsComponent implements OnInit {
+export class PostFormsComponent {
 title = "socialPosts";
-@Input() show: boolean;
 
+@Input() show: boolean;
 @Output() submitted = new EventEmitter<Posting>();
 @Output() showEvent = new EventEmitter<boolean>();
-// @Input() form: boolean;
+@Input() form: boolean;
 
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-  // addPost = (subject: string, body: string ) => {
-    
-  //   if (subject && body){
-
-
-  //     this.submitted.emit({subject, body});
-      
-      
-  //   }
-  // }
 
   addPost = (subject: string, body: string ) => {  
   if (body && subject){
 
-
-      this.submitted.emit({subject,body});
+  this.submitted.emit({subject,body});
+      
   }
       
     }
 
   hideForm = () => {
     this.showEvent.emit(false);
+    this.show=false;
   }
 }
--- src/app/socialPosts/socialPosts.component.ts
+++ src/app/socialPosts/socialPosts.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Posting } from '../postForms/postForms.component'
 @Component({
   selector: 'socialPosts',
   templateUrl: './socialPosts.component.html',
   styleUrls: ['./socialPosts.component.css']
 })
-export class SocialPostsComponent implements OnInit {
+export class SocialPostsComponent {
 title='My Thoughts';
 
 show: boolean = false;
@@ -18,14 +18,13 @@ posts:Posting[] =[
   { subject: 'Cow', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
   { subject: 'Whale', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' }
 ];
-  constructor() { }
 
-  ngOnInit() {
-  }
 onSubmit = (event) => {
   this.posts.unshift(event);
 }
-
+onShowEvent = (event) => {
+  this.show = event;
+}
 showForm = () => {
     this.show = true;
 }
